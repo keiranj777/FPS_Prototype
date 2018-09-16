fd3ce4552fa1475ef4ebdb890e5465db071491bd
Keiran Jarvis <keiranj777@hotmail.com>
2018-09-16 03:09:47 +0100

    Content hierarchy restructured, C++ renamed and refactored to run.

--- Source/FPS_Prototype/FPS_PrototypeGameMode.cpp
+++ Source/FPS_Prototype/FPS_PrototypeGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "FPS_PrototypeGameMode.h"
 #include "FPS_PrototypeHUD.h"
-#include "FPS_PrototypeCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFPS_PrototypeGameMode::AFPS_PrototypeGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
